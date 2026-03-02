d324c7946a7f7182a47f32524605d5907dd14d77
AndersonLoop <coellojanderson@gmail.com>
2026-03-02 10:57:57 -0500

    Modificar el .gitignore

--- src/main/java/ConsultaAPI.java
+++ src/main/java/ConsultaAPI.java
@@ -1,2 +1,35 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsultaAPI {
+
+    public Moneda buscarPelicula(int TipoMoneda){
+
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/1faa560f170e7b7d7226a9e9/latest/"+TipoMoneda);
+
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.n ewBuilder()
+                .uri(direccion)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(),Moneda.class);
+
+        } catch (Exception e) {
+            throw new RuntimeException("No encontre esa Moneda.");
+        }
+
+    }
+}
+
+
+
 }
--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenido al convertidor de Monedas usando la API exchangerate-api, a continuacion ingrese la Moneda inicial, la moneda que tiene y que quiere convertir a otra moneda");
+
+
+    }
 }
